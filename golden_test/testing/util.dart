31a48ce3189b56eeedec276eb65c03a8794aed94
Per Classon <perc@google.com>
2020-04-28 15:48:45 +0200

    Add golden tests for demo page (#127)
    * Add golden tests for demo page
    
    * Remove unused imports
    
    * Rename tests to make it clear that we are testing the demo page, and not the banner component

--- golden_test/testing/util.dart
+++ golden_test/testing/util.dart
@@ -7,6 +7,20 @@ import 'package:flutter_test/flutter_test.dart';
 
 const mobileSize = Size(540, 960);
 const desktopSize = Size(1280, 850);
+const homeAssets = [
+  'assets/icons/cupertino/cupertino.png',
+  'assets/icons/material/material.png',
+  'assets/icons/reference/reference.png',
+  'assets/logo/flutter_logo.png',
+  'assets/studies/crane_card.png',
+  'assets/studies/crane_card_dark.png',
+  'assets/studies/rally_card.png',
+  'assets/studies/rally_card_dark.png',
+  'assets/studies/shrine_card.png',
+  'assets/studies/shrine_card_dark.png',
+  'assets/studies/fortnightly_card.png',
+  'assets/studies/fortnightly_card_dark.png',
+];
 
 Future<void> setUpBinding(
   WidgetTester tester, {
