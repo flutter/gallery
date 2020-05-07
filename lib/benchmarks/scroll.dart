0986a44674ed28811ebf3f970376a7e6b92c9edd
Tianguang Zhang <penn.zht@gmail.com>
2020-05-07 17:02:53 +0200

    Remove deprecated.

--- lib/benchmarks/scroll.dart
+++ lib/benchmarks/scroll.dart
@@ -8,13 +8,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-@deprecated
-Offset absoluteTopLeftAlternate(RenderObject renderObject) {
-  final translation = renderObject.getTransformTo(null).getTranslation();
-
-  return Offset(translation.x, translation.y);
-}
-
 Offset absoluteTopLeft(RenderObject renderObject)
     => (renderObject as RenderBox).localToGlobal(Offset.zero);
 
@@ -102,70 +95,4 @@ Future<void> animationStops() async {
   });
 
   await stopped.future;
-}
-
-@deprecated
-Future<void> deprecatedScrollUntilVisible(
-    ScrollableState scrollableState,
-    Finder item,
-    {
-      bool elastic = false,
-      Size referenceSize,
-    }
-    ) async {
-  assert(scrollableState != null);
-  assert(item != null);
-
-  if (elastic) {
-    final double endOfScroll = scrollableState.position.maxScrollExtent;
-    scrollableState.position.jumpTo(endOfScroll);
-    await Future<void>.delayed(const Duration(milliseconds: 50));
-
-    return;
-  }
-
-  if (item.evaluate().isNotEmpty) {
-    // No need to scroll if already visible.
-    return;
-  }
-
-  bool isVisible = false;
-  Timer.periodic(const Duration(milliseconds: 50), (timer) {
-    if (item.evaluate().isNotEmpty) {
-      isVisible = true;
-      timer.cancel(); // TODO: remove this if it works unexpectedly.
-    }
-  });
-
-  while (!isVisible) {
-    // TODO: Warning! Experimental.
-    // TODO: scroll the scrollable.
-
-    // get current position
-    final double pixels = scrollableState.position.pixels;
-
-    // compute new position
-    final double newPixels = pixels + 30;
-
-    // jump to
-    scrollableState.position.jumpTo(newPixels);
-
-    // TODO: await a certain time.
-    await Future<void>.delayed(const Duration(milliseconds: 50));
-  }
-
-  // scroll up a little bit, just to make sure we can access the button.
-  // TODO: replace with smoother animation.
-
-  print('scrollUntilVisible | Scrolled.');
-
-  scrollableState.position.jumpTo(
-    scrollableState.position.pixels + referenceSize.height,
-  );
-
-  print('scrollUntilVisible | Scrolled more.');
-
-  await Future<void>.delayed(const Duration(milliseconds: 50));
-
-  return;
 }
