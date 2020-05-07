5d18820c483991f926b5a0834cd8b027b00af3d8
Tianguang Zhang <penn.zht@gmail.com>
2020-05-07 16:37:24 +0200

    Deprecate.

--- lib/benchmarks/scroll.dart
+++ lib/benchmarks/scroll.dart
@@ -8,7 +8,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-// TODO: Remove.
+@deprecated
 Offset absoluteTopLeftAlternate(RenderObject renderObject) {
   final translation = renderObject.getTransformTo(null).getTranslation();
 
