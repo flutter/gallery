852ded4edc848b77c194e10a1260f90e7f71ff1c
Tianguang Zhang <penn.zht@gmail.com>
2020-05-07 11:27:02 +0200

    Remove unused import.

--- lib/benchmarks/perf.dart
+++ lib/benchmarks/perf.dart
@@ -6,7 +6,6 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'recorder.dart';
