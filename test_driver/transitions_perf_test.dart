afba59e4e6309ab80e5c392abdf8e0ee79cbe06e
Tianguang Zhang <penn.zht@gmail.com>
2020-05-19 22:58:42 +0200

    Add TODOs.

--- test_driver/transitions_perf_test.dart
+++ test_driver/transitions_perf_test.dart
@@ -11,6 +11,8 @@ import 'package:test/test.dart' hide TypeMatcher, isInstanceOf;
 
 import 'isolates_workaround.dart';
 
+// TODO: update demo descriptions
+// TODO: add asserts for demos, making sure they are a subset of describe.
 // Demos for which timeline data will be collected using
 // FlutterDriver.traceAction().
 //
@@ -117,6 +119,7 @@ Future<void> runDemos(List<String> demos, FlutterDriver driver) async {
 
     print('> $demo');
 
+    // TODO: use lastIndexOf
     final demoCategory = demo.substring(demo.indexOf('@') + 1);
     if (demoCategory != currentDemoCategory) {
       // We've switched categories.
