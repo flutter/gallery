0b5769d52ee1749ba21ac7511f0f31eab1147c54
MH Johnson <johnsonmh@users.noreply.github.com>
2020-07-30 16:31:36 -0400

    Ignore deprecated use of BottomNavigationBarItem.title  (#247)
    * add ignore deprecated member use comments
    
    * update code viewer

--- lib/demos/material/bottom_navigation_demo.dart
+++ lib/demos/material/bottom_navigation_demo.dart
@@ -46,30 +46,35 @@ class _BottomNavigationDemoState extends State<BottomNavigationDemo> {
     var bottomNavigationBarItems = <BottomNavigationBarItem>[
       BottomNavigationBarItem(
         icon: const Icon(Icons.add_comment),
+        // ignore: deprecated_member_use
         title: Text(
           GalleryLocalizations.of(context).bottomNavigationCommentsTab,
         ),
       ),
       BottomNavigationBarItem(
         icon: const Icon(Icons.calendar_today),
+        // ignore: deprecated_member_use
         title: Text(
           GalleryLocalizations.of(context).bottomNavigationCalendarTab,
         ),
       ),
       BottomNavigationBarItem(
         icon: const Icon(Icons.account_circle),
+        // ignore: deprecated_member_use
         title: Text(
           GalleryLocalizations.of(context).bottomNavigationAccountTab,
         ),
       ),
       BottomNavigationBarItem(
         icon: const Icon(Icons.alarm_on),
+        // ignore: deprecated_member_use
         title: Text(
           GalleryLocalizations.of(context).bottomNavigationAlarmTab,
         ),
       ),
       BottomNavigationBarItem(
         icon: const Icon(Icons.camera_enhance),
+        // ignore: deprecated_member_use
         title: Text(
           GalleryLocalizations.of(context).bottomNavigationCameraTab,
         ),
