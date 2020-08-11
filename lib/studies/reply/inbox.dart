8c5bce52fa7dfc79b89f0df37aa48728a08e85ef
Renzo Olivares <renzo.olivares1@gmail.com>
2020-08-11 13:45:09 -0700

    Implement action bar and attack mail card to open container transform

--- lib/studies/reply/inbox.dart
+++ lib/studies/reply/inbox.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:gallery/layout/adaptive.dart';
 import 'package:gallery/studies/reply/mail_card_preview.dart';
 
 class InboxPage extends StatelessWidget {
-  const InboxPage();
+  const InboxPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final isDesktop = isDisplayDesktop(context);
+
     return Center(
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Expanded(
-            child: ListView(
-              padding: const EdgeInsetsDirectional.only(
-                  start: 120, end: 120, top: 28),
-              children: const [
-                MailCardPreview(),
-              ],
+            child: ListView.separated(
+              padding: EdgeInsetsDirectional.only(
+                start: isDesktop ? 120 : 6,
+                end: isDesktop ? 120 : 6,
+                top: isDesktop ? 28 : 6,
+              ),
+              itemCount: 2,
+              separatorBuilder: (context, index) => const SizedBox(height: 6),
+              itemBuilder: (context, index) {
+                return const MailCardPreview();
+              },
             ),
           ),
         ],
--- lib/studies/reply/mail_card_preview.dart
+++ lib/studies/reply/mail_card_preview.dart
@@ -1,59 +1,113 @@
 import 'package:flutter/material.dart';
+import 'package:animations/animations.dart';
+import 'package:gallery/layout/adaptive.dart';
+import 'package:gallery/studies/reply/colors.dart';
+import 'package:gallery/studies/reply/mail_view_page.dart';
+
+const _assetsPackage = 'flutter_gallery_assets';
+const _iconAssetLocation = 'reply/icons';
 
 class MailCardPreview extends StatelessWidget {
   const MailCardPreview({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shape: const RoundedRectangleBorder(
+    final isDesktop = isDisplayDesktop(context);
+    final textTheme = Theme.of(context).textTheme;
+
+    return OpenContainer(
+      openBuilder: (context, closedContainer) {
+        return const MailViewPage();
+      },
+      closedShape: const RoundedRectangleBorder(
         borderRadius: BorderRadius.all(Radius.circular(0)),
       ),
-      child: Row(
-        children: [
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Row(
+      closedElevation: 0,
+      closedBuilder: (context, openContainer) {
+        return InkWell(
+          onTap: openContainer,
+          child: Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
+                      Row(
+                        children: [
+                          Text('Google Express', style: textTheme.caption),
+                          Padding(
+                            padding: const EdgeInsetsDirectional.only(
+                              start: 4,
+                              end: 4,
+                            ),
+                            child: Container(
+                              color: Colors.black,
+                              width: 12,
+                              height: 0.5,
+                            ),
+                          ),
+                          Text('15 mins ago', style: textTheme.caption),
+                        ],
+                      ),
+                      const SizedBox(height: 4),
+                      Text('Package Shipped!', style: textTheme.headline6),
+                      const SizedBox(height: 16),
                       Text(
-                        'Google Express',
-                        style: Theme.of(context).textTheme.caption,
-                      ),
-                      Padding(
-                        padding:
-                            const EdgeInsetsDirectional.only(start: 4, end: 4),
-                        child: Container(
-                          color: Colors.black,
-                          width: 12,
-                          height: 0.5,
-                        ),
-                      ),
-                      Text(
-                        '15 mins ago',
-                        style: Theme.of(context).textTheme.caption,
-                      ),
+                        'Cucumber Mask Facial has shipped',
+                        style: textTheme.bodyText2,
+                      )
                     ],
                   ),
-                  const SizedBox(height: 4),
-                  Text(
-                    'Package Shipped!',
-                    style: Theme.of(context).textTheme.headline5,
-                  ),
-                  const SizedBox(height: 16),
-                  Text(
-                    'Cucumber Mask Facial has shipped',
-                    style: Theme.of(context).textTheme.bodyText2,
-                  )
-                ],
+                ),
               ),
-            ),
+              Transform.translate(
+                offset: const Offset(-20, -24),
+                child: Row(
+                  children: [
+                    if (isDesktop)
+                      Row(
+                        children: const [
+                          ImageIcon(
+                            AssetImage(
+                              '$_iconAssetLocation/twotone_star.png',
+                              package: _assetsPackage,
+                            ),
+                            color: ReplyColors.blue600,
+                          ),
+                          SizedBox(width: 20),
+                          ImageIcon(
+                            AssetImage(
+                              '$_iconAssetLocation/twotone_delete.png',
+                              package: _assetsPackage,
+                            ),
+                            color: ReplyColors.blue600,
+                          ),
+                          SizedBox(width: 20),
+                          Icon(Icons.more_vert, color: ReplyColors.blue700),
+                        ],
+                      ),
+                    const SizedBox(width: 20),
+                    Transform.translate(
+                      offset: Offset(0, isDesktop ? -18.5 : 0),
+                      child: ClipOval(
+                        child: Image.asset(
+                          'reply/avatars/avatar_0.jpg',
+                          package: _assetsPackage,
+                          height: 36,
+                          width: 36,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
