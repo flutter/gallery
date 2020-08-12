56820de2b7d7a317c96bf6967e091e5a680b9299
Renzo Olivares <renzo.olivares1@gmail.com>
2020-08-11 19:28:35 -0700

    Layout changes

--- lib/studies/reply/mail_view_page.dart
+++ lib/studies/reply/mail_view_page.dart
@@ -5,10 +5,50 @@ class MailViewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: OutlineButton(
-          onPressed: () => Navigator.pop(context),
-          child: const Text('Back'),
+      body: SafeArea(
+        child: Padding(
+          padding:
+              const EdgeInsetsDirectional.only(top: 32, start: 20, end: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      'Package shipped!',
+                      style: Theme.of(context).textTheme.headline4,
+                    ),
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.keyboard_arrow_down),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          const Text('Google Express'),
+                          Container(),
+                          const Text('4 hrs ago'),
+                        ],
+                      ),
+                      const Text('To Jeff,'),
+                    ],
+                  ),
+                ],
+              ),
+              const Text('Message'),
+            ],
+          ),
         ),
       ),
     );
