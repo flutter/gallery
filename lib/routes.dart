f1b13b5bfb072f784b99a927a31c83701d5fa146
Renzo Olivares <renzo.olivares1@gmail.com>
2020-08-21 00:50:17 -0700

    Moves alignment of Back Button on Reply study from bottomStart to topCenter * Allows for bottom drawer toggle to be visible and clickable on mobile

--- lib/routes.dart
+++ lib/routes.dart
@@ -5,8 +5,8 @@ import 'package:gallery/pages/demo.dart';
 import 'package:gallery/pages/home.dart';
 import 'package:gallery/studies/crane/app.dart';
 import 'package:gallery/studies/fortnightly/app.dart';
-import 'package:gallery/studies/reply/app.dart';
 import 'package:gallery/studies/rally/app.dart';
+import 'package:gallery/studies/reply/app.dart';
 import 'package:gallery/studies/shrine/app.dart';
 import 'package:gallery/studies/starter/app.dart';
 
@@ -60,7 +60,10 @@ class RouteConfiguration {
     ),
     Path(
       r'^' + ReplyApp.homeRoute,
-      (context, match) => const StudyWrapper(study: ReplyApp()),
+      (context, match) => const StudyWrapper(
+        alignment: AlignmentDirectional.topCenter,
+        study: ReplyApp(),
+      ),
     ),
     Path(
       r'^' + StarterApp.defaultRoute,
