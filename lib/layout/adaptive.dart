ff0d5c34c9e0fe54267934581394b025a6365333
Jose Alba <jaguado@google.com>
2020-08-24 13:04:42 +0200

    Use adaptive breakpoints package for adaptive experience

--- lib/layout/adaptive.dart
+++ lib/layout/adaptive.dart
@@ -3,40 +3,18 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+import 'package:adaptive_breakpoints/adaptive_breakpoints.dart';
 
-enum DisplayType {
-  desktop,
-  mobile,
-}
-
-const _desktopPortraitBreakpoint = 700.0;
-const _desktopLandscapeBreakpoint = 1000.0;
-
-/// Returns the [DisplayType] for the current screen. This app only supports
-/// mobile and desktop layouts, and as such we only have one breakpoint.
-DisplayType displayTypeOf(BuildContext context) {
-  final orientation = MediaQuery.of(context).orientation;
-  final width = MediaQuery.of(context).size.width;
-
-  if ((orientation == Orientation.landscape &&
-          width > _desktopLandscapeBreakpoint) ||
-      (orientation == Orientation.portrait &&
-          width > _desktopPortraitBreakpoint)) {
-    return DisplayType.desktop;
-  } else {
-    return DisplayType.mobile;
-  }
-}
-
-/// Returns a boolean if we are in a display of [DisplayType.desktop]. Used to
+/// Returns a boolean if we are in a display is a medium or larger screen. Used to
 /// build adaptive and responsive layouts.
 bool isDisplayDesktop(BuildContext context) {
-  return displayTypeOf(context) == DisplayType.desktop;
+  final windowType = getWindowType(context);
+  return (windowType == AdaptiveWindowType.m ||
+      windowType == AdaptiveWindowType.l ||
+      windowType == AdaptiveWindowType.xl);
 }
 
-/// Returns a boolean if we are in a display of [DisplayType.desktop] but less
-/// than [_desktopLandscapeBreakpoint] width. Used to build adaptive and responsive layouts.
+/// Returns true if the window size is medium size. Used to build adaptive and responsive layouts.
 bool isDisplaySmallDesktop(BuildContext context) {
-  return isDisplayDesktop(context) &&
-      MediaQuery.of(context).size.width < _desktopLandscapeBreakpoint;
+  return getWindowType(context) == AdaptiveWindowType.m;
 }
