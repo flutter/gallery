112c1c93072e952bf44ea5ee6dcc5cb89d3adf3e
Renzo Olivares <renzo.olivares1@gmail.com>
2020-08-25 10:34:13 -0700

    Workaround for mobile fab open container a11y states issue

--- lib/studies/reply/adaptive_nav.dart
+++ lib/studies/reply/adaptive_nav.dart
@@ -1081,11 +1081,15 @@ class _ReplyFab extends StatefulWidget {
 class _ReplyFabState extends State<_ReplyFab>
     with SingleTickerProviderStateMixin {
   static final fabKey = UniqueKey();
+  static const double _mobileFabDimension = 56;
 
   @override
   Widget build(BuildContext context) {
     final isDesktop = isDisplayDesktop(context);
     final theme = Theme.of(context);
+    final mobileFabBorder = const RoundedRectangleBorder(
+      borderRadius: BorderRadius.all(Radius.circular(_mobileFabDimension / 2)),
+    );
 
     return OpenContainer(
       openBuilder: (context, closedContainer) {
@@ -1094,17 +1098,25 @@ class _ReplyFabState extends State<_ReplyFab>
       openColor: theme.cardColor,
       closedShape: isDesktop & widget.extended
           ? const StadiumBorder()
-          : const CircleBorder(),
+          : isDesktop ? const CircleBorder() : mobileFabBorder,
       closedColor: theme.colorScheme.secondary,
+      closedElevation: 6,
       closedBuilder: (context, openContainer) {
         return Consumer<EmailStore>(
           builder: (context, model, child) {
             final onMailView = model.onMailView;
             final fabSwitcher = _FadeThroughTransitionSwitcher(
-              fillColor: Theme.of(context).colorScheme.secondary,
+              fillColor: Colors.transparent,
               child: onMailView
-                  ? Icon(Icons.reply_all, key: fabKey)
-                  : const Icon(Icons.create),
+                  ? Icon(
+                      Icons.reply_all,
+                      key: fabKey,
+                      color: Colors.black,
+                    )
+                  : const Icon(
+                      Icons.create,
+                      color: Colors.black,
+                    ),
             );
             final tooltip = onMailView ? 'Reply' : 'Compose';
 
@@ -1135,11 +1147,16 @@ class _ReplyFabState extends State<_ReplyFab>
                 ),
               );
             } else {
-              return FloatingActionButton(
-                heroTag: 'Bottom App Bar FAB',
-                tooltip: tooltip,
-                child: fabSwitcher,
-                onPressed: openContainer,
+              return InkWell(
+                customBorder: mobileFabBorder,
+                onTap: openContainer,
+                child: SizedBox(
+                  height: _mobileFabDimension,
+                  width: _mobileFabDimension,
+                  child: Center(
+                    child: fabSwitcher,
+                  ),
+                ),
               );
             }
           },
