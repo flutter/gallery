24f0438e3ca2e5605d475e27c4f509603c96f32d
Renzo Olivares <rmolivares@renzo-olivares.dev>
2020-09-17 09:45:43 -0700

    Make drawer shade and mailbox title fade to be in sync with drawer animation (#310)

--- lib/studies/reply/adaptive_nav.dart
+++ lib/studies/reply/adaptive_nav.dart
@@ -642,10 +642,8 @@ class _MobileNavState extends State<_MobileNav> with TickerProviderStateMixin {
             maintainAnimation: true,
             maintainState: true,
             visible: _bottomDrawerVisible,
-            child: AnimatedOpacity(
-              opacity: _bottomDrawerVisible ? 1.0 : 0.0,
-              curve: standardEasing,
-              duration: _kAnimationDuration,
+            child: FadeTransition(
+              opacity: _drawerCurve,
               child: Container(
                 height: MediaQuery.of(context).size.height,
                 width: MediaQuery.of(context).size.width,
@@ -729,6 +727,10 @@ class _AnimatedBottomAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var fadeOut = Tween<double>(begin: 1, end: -1).animate(
+      drawerController.drive(CurveTween(curve: standardEasing)),
+    );
+
     return Selector<EmailStore, bool>(
       selector: (context, emailStore) => emailStore.onMailView,
       builder: (context, onMailView, child) {
@@ -768,18 +770,21 @@ class _AnimatedBottomAppBar extends StatelessWidget {
                           const SizedBox(width: 8),
                           const _ReplyLogo(),
                           const SizedBox(width: 10),
-                          AnimatedOpacity(
-                            opacity:
-                                bottomDrawerVisible || onMailView ? 0.0 : 1.0,
-                            duration: _kAnimationDuration,
-                            curve: standardEasing,
-                            child: Text(
-                              navigationDestinations[selectedIndex].name,
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodyText1
-                                  .copyWith(color: ReplyColors.white50),
-                            ),
+                          _FadeThroughTransitionSwitcher(
+                            fillColor: Colors.transparent,
+                            child: onMailView
+                                ? const SizedBox(width: 48)
+                                : FadeTransition(
+                                    opacity: fadeOut,
+                                    child: Text(
+                                      navigationDestinations[selectedIndex]
+                                          .name,
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodyText1
+                                          .copyWith(color: ReplyColors.white50),
+                                    ),
+                                  ),
                           ),
                         ],
                       ),
