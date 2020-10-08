0dbbe791013307e0af8dee93a3473ac4da01698c
Alabhya Singh <singhalabhya32@gmail.com>
2020-10-08 21:26:52 +0530

    Added trailing comma and removed redundant code

--- lib/demos/material/navigation_drawer.dart
+++ lib/demos/material/navigation_drawer.dart
@@ -15,28 +15,33 @@ class NavDrawerDemo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var localization = GalleryLocalizations.of(context);
     final drawerHeader = UserAccountsDrawerHeader(
-      accountName:
-          Text(GalleryLocalizations.of(context).demoNavigationDrawerUserName),
-      accountEmail:
-          Text(GalleryLocalizations.of(context).demoNavigationDrawerUserEmail),
+      accountName: Text(
+        localization.demoNavigationDrawerUserName,
+      ),
+      accountEmail: Text(
+        localization.demoNavigationDrawerUserEmail,
+      ),
       currentAccountPicture: const CircleAvatar(
         child: FlutterLogo(size: 42.0),
       ),
     );
     final drawerItems = ListView(
-      children: <Widget>[
+      children: [
         drawerHeader,
         ListTile(
           title: Text(
-              GalleryLocalizations.of(context).demoNavigationDrawerToPageOne),
+            localization.demoNavigationDrawerToPageOne,
+          ),
           onTap: () {
             Navigator.pop(context);
           },
         ),
         ListTile(
           title: Text(
-              GalleryLocalizations.of(context).demoNavigationDrawerToPageTwo),
+            localization.demoNavigationDrawerToPageTwo,
+          ),
           onTap: () {
             Navigator.pop(context);
           },
@@ -45,10 +50,17 @@ class NavDrawerDemo extends StatelessWidget {
     );
     return Scaffold(
       appBar: AppBar(
-        title: Text(GalleryLocalizations.of(context).demoNavigationDrawerTitle),
+        title: Text(
+          localization.demoNavigationDrawerTitle,
+        ),
       ),
       body: Center(
-        child: Text(GalleryLocalizations.of(context).demoNavigationDrawerText),
+        child: Padding(
+          padding: const EdgeInsets.all(50.0),
+          child: Text(
+            localization.demoNavigationDrawerText,
+          ),
+        ),
       ),
       drawer: Drawer(
         child: drawerItems,
