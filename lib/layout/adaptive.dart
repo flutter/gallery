2b3dec2cce38f8e4926c0d3e16b5d4c95f1edf03
gozeloglu <gozeloglu@gmail.com>
2020-10-17 09:57:27 +0000

    changed the comment lines to dart style I took https://github.com/flutter/gallery/issues/168 issue on me and changed the comment lines to make dart style

--- lib/layout/adaptive.dart
+++ lib/layout/adaptive.dart
@@ -5,11 +5,15 @@
 import 'package:flutter/material.dart';
 import 'package:adaptive_breakpoints/adaptive_breakpoints.dart';
 
+/// Returns a boolean by screen size.
+///
 /// Returns a boolean if we are on a medium or larger screen. Used to
 /// build adaptive and responsive layouts.
 bool isDisplayDesktop(BuildContext context) =>
     getWindowType(context) >= AdaptiveWindowType.m;
 
+/// Returns a boolean by window size 
+///
 /// Returns true if the window size is medium size. Used to build adaptive and responsive layouts.
 bool isDisplaySmallDesktop(BuildContext context) {
   return getWindowType(context) == AdaptiveWindowType.m;
