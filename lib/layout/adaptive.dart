8a36b2347eb09790f929ec63c54ccc0ab0390c0c
Pierre-Louis <plg@google.com>
2020-10-19 16:37:00 +0200

    updates

--- lib/layout/adaptive.dart
+++ lib/layout/adaptive.dart
@@ -8,9 +8,9 @@ import 'package:adaptive_breakpoints/adaptive_breakpoints.dart';
 /// Returns a boolean if we are on a medium or larger screen. Used to
 /// build adaptive and responsive layouts.
 bool isDisplayDesktop(BuildContext context) =>
-    getWindowType(context) >= AdaptiveWindowType.m;
+    getWindowType(context) >= AdaptiveWindowType.medium;
 
 /// Returns true if the window size is medium size. Used to build adaptive and responsive layouts.
 bool isDisplaySmallDesktop(BuildContext context) {
-  return getWindowType(context) == AdaptiveWindowType.m;
+  return getWindowType(context) == AdaptiveWindowType.medium;
 }
