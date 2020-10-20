5c27f14056c270a1ac3909900a4cf335b59a28ac
Pierre-Louis <plg@google.com>
2020-10-20 11:27:43 +0200

    Update transitions_perf_test.dart

--- test_driver/transitions_perf_test.dart
+++ test_driver/transitions_perf_test.dart
@@ -13,7 +13,7 @@ import 'package:test/test.dart' hide TypeMatcher, isInstanceOf;
 // flutter drive --profile --trace-startup -t test_driver/transitions_perf.dart -d <device>
 // To run this test for just Crane, with scrolling:
 // flutter drive --profile --trace-startup -t test_driver/transitions_perf.dart -d <device> --dart-define=onlyCrane=true
-// To run this test for just Crane, with animations:
+// To run this test for just Rally, with animations:
 // flutter drive --profile --trace-startup -t test_driver/transitions_perf.dart -d <device> --dart-define=onlyReply=true
 
 // Demos for which timeline data will be collected using
@@ -269,7 +269,7 @@ void main([List<String> args = const <String>[]]) {
     test('only Reply', () async {
       if (!isTestingReplyOnly) return;
 
-      // Collect timeline data for just the Crane study.
+      // Collect timeline data for just the Reply study.
       final timeline = await driver.traceAction(
         () async {
           await runDemos(
