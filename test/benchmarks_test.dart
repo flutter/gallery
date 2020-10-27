18e2eb8bec48b27d845892389b9c7074aab022ce
Tianguang Zhang <penn.zht@gmail.com>
2020-10-27 11:35:32 +0100

    Add print statements.

--- test/benchmarks_test.dart
+++ test/benchmarks_test.dart
@@ -4,11 +4,15 @@ import 'dart:io';
 import 'package:web_benchmarks/server.dart';
 
 Future<void> main() async {
+  print ('Starting...');
+
   final taskResult = await serveWebBenchmark(
-    benchmarkAppDirectory: Directory('.'),
+    benchmarkAppDirectory: Directory.current,
     entryPoint: 'lib/benchmarks/client.dart',
     useCanvasKit: false,
   );
+
+  print ('Done.');
 
   print ((const JsonEncoder.withIndent('  ')).convert(taskResult.toJson()));
 }
