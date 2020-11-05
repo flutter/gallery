29759aae70e5af9e134c3c225823e23344808b98
Tianguang Zhang <penn.zht@gmail.com>
2020-11-05 22:48:15 +0100

    Add test.

--- test/benchmarks_test.dart
+++ test/benchmarks_test.dart
@@ -1,19 +1,51 @@
-import 'dart:convert';
-import 'dart:io';
+import 'dart:convert' show JsonEncoder;
 
-import 'package:web_benchmarks/server.dart';
+import 'package:test/test.dart';
+
+import 'package:gallery/benchmarks/client.dart' as client;
+import 'package:gallery/benchmarks/run_benchmarks.dart';
 
 Future<void> main() async {
-  print ('Starting web benchmark tests ...');
+  test('Can run a web benchmark', () async {
+    print ('Starting web benchmark tests ...');
 
-  final taskResult = await serveWebBenchmark(
-    benchmarkAppDirectory: Directory.current.absolute,
-    entryPoint: 'lib/benchmarks/client.dart',
-    useCanvasKit: false,
-  );
+    final taskResult = await serveGalleryWebBenchmarks();
 
-  print ('Web benchmark tests finished.');
+    print ('Web benchmark tests finished.');
 
-  print ((const JsonEncoder.withIndent('  ')).convert(taskResult.toJson()));
+    for (final benchmarkName in client.benchmarks.keys) {
+      for (final metricName in <String>[
+        'preroll_frame',
+        'apply_frame',
+        'drawFrameDuration',
+      ]) {
+        for (final valueName in <String>[
+          'average',
+          'outlierAverage',
+          'outlierRatio',
+          'noise',
+        ]) {
+          expect(
+            taskResult.scores[benchmarkName].where(
+              (score) => score.metric == '$metricName.$valueName',
+            ),
+            hasLength(1),
+          );
+        }
+      }
+
+      expect(
+        taskResult.scores[benchmarkName].where(
+          (score) => score.metric == 'totalUiFrame.average',
+        ),
+        hasLength(1),
+      );
+    }
+
+    expect(
+      const JsonEncoder.withIndent('  ').convert(taskResult.toJson()),
+      isA<String>(),
+    );
+  }, timeout: Timeout.none);
 }
 
