35a633ed6ce418ea5348446f7f4899ba74e5d9a2
Tianguang Zhang <penn.zht@gmail.com>
2020-11-06 16:45:42 +0100

    Move to common.dart.

--- lib/benchmarks/client.dart
+++ lib/benchmarks/client.dart
@@ -1,29 +1,6 @@
 import 'package:web_benchmarks/client.dart';
 
 import 'common.dart';
-import 'gallery_automator.dart';
-import 'gallery_recorder.dart';
-
-typedef RecorderFactory = Recorder Function();
-
-final Map<String, RecorderFactory> benchmarks = <String, RecorderFactory>{
-  galleryStudiesPerf: () => GalleryRecorder(
-    benchmarkName: galleryStudiesPerf,
-    shouldRunPredicate: (demo) => typeOfDemo(demo) == DemoType.study,
-  ),
-  galleryUnanimatedPerf: () => GalleryRecorder(
-    benchmarkName: galleryUnanimatedPerf,
-    shouldRunPredicate: (demo) => typeOfDemo(demo) == DemoType.unanimatedWidget,
-  ),
-  galleryAnimatedPerf: () => GalleryRecorder(
-    benchmarkName: galleryAnimatedPerf,
-    shouldRunPredicate: (demo) => typeOfDemo(demo) == DemoType.animatedWidget,
-  ),
-  galleryScrollPerf: () => GalleryRecorder(
-    benchmarkName: galleryScrollPerf,
-    testScrollingOnly: true,
-  ),
-};
 
 // Run benchmarks.
 Future<void> main() async {
--- lib/benchmarks/common.dart
+++ lib/benchmarks/common.dart
@@ -1,7 +1,33 @@
+import 'package:web_benchmarks/client.dart';
+
+import 'gallery_automator.dart';
+import 'gallery_recorder.dart';
+
 const String galleryBenchmarkPrefix = 'gallery_v2';
 
 const String galleryStudiesPerf = '${galleryBenchmarkPrefix}_studies_perf';
 const String galleryUnanimatedPerf = '${galleryBenchmarkPrefix}_unanimated_perf';
 const String galleryAnimatedPerf = '${galleryBenchmarkPrefix}_animated_perf';
 const String galleryScrollPerf = '${galleryBenchmarkPrefix}_scroll_perf';
+
+typedef RecorderFactory = Recorder Function();
+
+final Map<String, RecorderFactory> benchmarks = <String, RecorderFactory>{
+  galleryStudiesPerf: () => GalleryRecorder(
+    benchmarkName: galleryStudiesPerf,
+    shouldRunPredicate: (demo) => typeOfDemo(demo) == DemoType.study,
+  ),
+  galleryUnanimatedPerf: () => GalleryRecorder(
+    benchmarkName: galleryUnanimatedPerf,
+    shouldRunPredicate: (demo) => typeOfDemo(demo) == DemoType.unanimatedWidget,
+  ),
+  galleryAnimatedPerf: () => GalleryRecorder(
+    benchmarkName: galleryAnimatedPerf,
+    shouldRunPredicate: (demo) => typeOfDemo(demo) == DemoType.animatedWidget,
+  ),
+  galleryScrollPerf: () => GalleryRecorder(
+    benchmarkName: galleryScrollPerf,
+    testScrollingOnly: true,
+  ),
+};
 
