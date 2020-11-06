a5c2b6058dae3b729719283c605583022a2aa292
Tianguang Zhang <penn.zht@gmail.com>
2020-11-06 19:27:22 +0100

    Add helper function for project root directory.

--- test/benchmarks/run_benchmarks.dart
+++ test/benchmarks/run_benchmarks.dart
@@ -1,14 +1,15 @@
 import 'dart:convert' show JsonEncoder;
-import 'dart:io';
 
 import 'package:web_benchmarks/server.dart';
+
+import 'project_root_directory.dart';
 
 /// Runs the Gallery web benchmarks and reports the benchmark data.
 Future<void> main() async {
   print('Starting web benchmark tests ...');
 
   final taskResult = await serveWebBenchmark(
-    benchmarkAppDirectory: Directory.current.absolute,
+    benchmarkAppDirectory: projectRootDirectory(),
     entryPoint: 'test/benchmarks/client.dart',
     useCanvasKit: false,
   );
--- test/benchmarks_test.dart
+++ test/benchmarks_test.dart
@@ -1,11 +1,10 @@
 import 'dart:convert' show JsonEncoder;
-import 'dart:io';
 
 import 'package:test/test.dart';
-import 'package:path/path.dart' as path;
 import 'package:web_benchmarks/server.dart';
 
 import 'benchmarks/common.dart';
+import 'benchmarks/project_root_directory.dart';
 
 final metricList = <String>[
   'preroll_frame',
@@ -20,40 +19,13 @@ final valueList = <String>[
   'noise',
 ];
 
-bool hasPubspec(Directory directory) {
-  return directory.listSync().any(
-        (entity) =>
-            FileSystemEntity.isFileSync(entity.path) &&
-            path.basename(entity.path) == 'pubspec.yaml',
-      );
-}
-
-Directory projectRootDirectory() {
-  var current = Directory.current.absolute;
-
-  while (!hasPubspec(current)) {
-    if (current.path == current.parent.path) {
-      throw Exception('Reached file system root when seeking project root.');
-    }
-
-    current = current.parent;
-  }
-
-  return current;
-}
-
 /// Tests that the Gallery web benchmarks are run and reported correctly.
 Future<void> main() async {
   test('Can run a web benchmark', () async {
     print('Starting web benchmark tests ...');
 
-    final rootDirectory = projectRootDirectory();
-
-    print('rootDirectory = $rootDirectory');
-    print('rootDirectory.path = ${rootDirectory.path}');
-
     final taskResult = await serveWebBenchmark(
-      benchmarkAppDirectory: rootDirectory,
+      benchmarkAppDirectory: projectRootDirectory(),
       entryPoint: 'test/benchmarks/client.dart',
       useCanvasKit: false,
     );
