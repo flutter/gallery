0348cd7626aee4ecd1512f941a494a6c0e079d70
Tianguang Zhang <penn.zht@gmail.com>
2020-11-07 20:56:29 +0100

    Add metrics.

---
+++ lib/icon/metrics.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+// Color gradient.
+const pinkLeft = Color(0xFFFF5983);
+const pinkRight = Color(0xFFFF8383);
+
+const tealLeft = Color(0xFF1CDDC8);
+const tealRight = Color(0xFF00A5B3);
+
+// Dimensions.
+const squareWidth = 1;
+
+const leftOffset = 1/5;
+const stickLength = 5/9;
+const stickWidth = 5/36;
+const knobDiameter = 5/54;
+const stickGap = 5/54;
+const verticalMargin = 17/54;
+
+// Durations.
+const knobExpansionDuration = 11;
+const colorKnobContractionDuration = 3;
+const crossoverDuration = 9;
+
