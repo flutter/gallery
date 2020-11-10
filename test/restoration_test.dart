8d2307c10b046eee09e961a22773a84c9cd82174
Shi Hao Hong <shihaohong94@gmail.com>
2020-11-11 01:22:53 +0800

    Flutter format

--- test/restoration_test.dart
+++ test/restoration_test.dart
@@ -7,80 +7,86 @@ import 'package:gallery/studies/reply/app.dart';
 import 'package:gallery/studies/reply/search_page.dart';
 
 void main() {
-  testWidgets('Restoration test - Reply', (tester) async {
-    await tester.pumpWidget(const GalleryApp());
-    // Let the splash page animations complete.
-    await tester.pump(const Duration(seconds: 1));
+  testWidgets(
+    'Restoration test - Reply',
+    (tester) async {
+      await tester.pumpWidget(const GalleryApp());
+      // Let the splash page animations complete.
+      await tester.pump(const Duration(seconds: 1));
 
-    expect(find.byType(HomePage), findsOneWidget);
+      expect(find.byType(HomePage), findsOneWidget);
 
-    await tester.tap(find.byKey(const ValueKey('reply@study')));
-    await tester.pumpAndSettle();
+      await tester.tap(find.byKey(const ValueKey('reply@study')));
+      await tester.pumpAndSettle();
 
-    // Should be on the reply study.
-    expect(find.byType(ReplyApp), findsOneWidget);
+      // Should be on the reply study.
+      expect(find.byType(ReplyApp), findsOneWidget);
 
-    await tester.restartAndRestore();
-    await tester.pump(const Duration(seconds: 1));
+      await tester.restartAndRestore();
+      await tester.pump(const Duration(seconds: 1));
 
-    // Should still be on the reply study after restoring state.
-    expect(find.byType(ReplyApp), findsOneWidget);
+      // Should still be on the reply study after restoring state.
+      expect(find.byType(ReplyApp), findsOneWidget);
 
-    // Should be on the inbox page.
-    expect(find.text('Package shipped!'), findsOneWidget);
+      // Should be on the inbox page.
+      expect(find.text('Package shipped!'), findsOneWidget);
 
-    // Navigate to the spam page.
-    await tester.tap(find.text('Inbox'));
-    await tester.pumpAndSettle();
-    await tester.tap(find.text('Spam'));
-    await tester.pumpAndSettle();
+      // Navigate to the spam page.
+      await tester.tap(find.text('Inbox'));
+      await tester.pumpAndSettle();
+      await tester.tap(find.text('Spam'));
+      await tester.pumpAndSettle();
 
-    // Should be on the spam page.
-    expect(find.text('Free money'), findsOneWidget);
+      // Should be on the spam page.
+      expect(find.text('Free money'), findsOneWidget);
 
-    await tester.restartAndRestore();
-    await tester.pump(const Duration(seconds: 1));
+      await tester.restartAndRestore();
+      await tester.pump(const Duration(seconds: 1));
 
-    // Should still be on the spam page after restoring state.
-    expect(find.text('Free money'), findsOneWidget);
+      // Should still be on the spam page after restoring state.
+      expect(find.text('Free money'), findsOneWidget);
 
-    await tester.tap(find.text('Free money'));
-    await tester.pumpAndSettle();
+      await tester.tap(find.text('Free money'));
+      await tester.pumpAndSettle();
 
-    // Star an item
-    await tester.tap(find.byKey(const ValueKey('star_email_button')));
+      // Star an item
+      await tester.tap(find.byKey(const ValueKey('star_email_button')));
 
-    // Navigate to starred mailbox
-    await tester.tap(find.byKey(const ValueKey('navigation_button')));
-    await tester.pumpAndSettle();
-    await tester.tap(find.text('Starred'));
-    await tester.pumpAndSettle();
+      // Navigate to starred mailbox
+      await tester.tap(find.byKey(const ValueKey('navigation_button')));
+      await tester.pumpAndSettle();
+      await tester.tap(find.text('Starred'));
+      await tester.pumpAndSettle();
 
-    // Recently starred email should be found.
-    expect(find.text('Free money'), findsOneWidget);
+      // Recently starred email should be found.
+      expect(find.text('Free money'), findsOneWidget);
 
-    await tester.restartAndRestore();
-    await tester.pump(const Duration(seconds: 1));
+      await tester.restartAndRestore();
+      await tester.pump(const Duration(seconds: 1));
 
-    // Should still by on the starred email page.
-    expect(find.text('Free money'), findsOneWidget);
+      // Should still by on the starred email page.
+      expect(find.text('Free money'), findsOneWidget);
 
-    await tester.tap(find.byKey(const ValueKey('ReplySearch')));
-    await tester.pumpAndSettle();
+      await tester.tap(find.byKey(const ValueKey('ReplySearch')));
+      await tester.pumpAndSettle();
 
-    // Open search page.
-    expect(find.byType(SearchPage), findsOneWidget);
+      // Open search page.
+      expect(find.byType(SearchPage), findsOneWidget);
 
-    await tester.restartAndRestore();
-    await tester.pump(const Duration(seconds: 1));
+      await tester.restartAndRestore();
+      await tester.pump(const Duration(seconds: 1));
 
-    // Should still by on the search page.
-    expect(find.byType(SearchPage), findsOneWidget);
+      // Should still by on the search page.
+      expect(find.byType(SearchPage), findsOneWidget);
 
-    await tester.tap(find.byKey(const ValueKey('ReplyExit')));
-    await tester.pumpAndSettle();
+      await tester.tap(find.byKey(const ValueKey('ReplyExit')));
+      await tester.pumpAndSettle();
 
-    // Should be on the starred email page instead of any other.
-    expect(find.text('Free money'), findsOneWidget);
-  }, variant: const TargetPlatformVariant(<TargetPlatform>{ TargetPlatform.android }));
+      // Should be on the starred email page instead of any other.
+      expect(find.text('Free money'), findsOneWidget);
+    },
+    variant: const TargetPlatformVariant(
+      <TargetPlatform>{TargetPlatform.android},
+    ),
+  );
 }
