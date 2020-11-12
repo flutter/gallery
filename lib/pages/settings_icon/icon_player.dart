c48e873c640e6296cadbbfa30e76e3c41ff0e541
Tianguang Zhang <penn.zht@gmail.com>
2020-11-12 16:17:47 +0100

    Add icon_player.dart.

--- lib/pages/settings_icon/icon_player.dart
+++ lib/pages/settings_icon/icon_player.dart
@@ -12,53 +12,71 @@ class IconDisplayer extends StatefulWidget {
   _IconDisplayerState createState() => _IconDisplayerState();
 }
 
-class _IconDisplayerState extends State<IconDisplayer>{
+class _IconDisplayerState extends State<IconDisplayer> with TickerProviderStateMixin{
   double _time = 0;
 
   final callbacks = <ValueChanged<double>>[];
+
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 2500),
+      vsync: this,
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Column(
-          children: [
-            Row(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.pink, width: 2),
-                  ),
-                  width: 600,
-                  height: 600,
-                  child: FlareDisplayer(callbacks: callbacks),
+        body: AnimatedBuilder(
+          animation: _controller,
+          builder: (BuildContext context, Widget child)
+    {
+      for (final callback in callbacks) {
+        callback(_translate(_controller.value));
+      }
+      return Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.pink, width: 2),
                 ),
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.pink, width: 2),
-                  ),
-                  width: 600,
-                  height: 600,
-                  child: SettingsIcon(_time),
+                width: 600,
+                height: 600,
+                child: FlareDisplayer(callbacks: callbacks),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.pink, width: 2),
                 ),
-              ],
-            ),
-            Slider(
-                min: 0,
-                max: 100,
-                value: _time * 100,
-                divisions: 100,
-                onChanged: (double value) {
-                  setState( () {
-                    print('value changed to $value');
-                    _time = value / 100;
-                    callbacks.single.call(_time);
-                  });
-                }
-            ),
-          ],
+                width: 600,
+                height: 600,
+                child: SettingsIcon(_translate(_controller.value)),
+              ),
+            ],
+          ),
+        ],
+      );
+    },
         ),
       ),
     );
   }
 }
+
+double _translate(double original) {
+  return original < 0.5 ? 2 * original : 2 - 2 * original;
+}
+
