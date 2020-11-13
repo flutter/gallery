39f6d0b149e5bf5a0123824956995e09b06b7ae8
Alabhya268 <singhalabhya32@gmail.com>
2020-11-13 08:01:13 +0530

    Added source code for top appbar

--- lib/demos/material/top_app_bar_demo.dart
+++ lib/demos/material/top_app_bar_demo.dart
@@ -1,3 +1,58 @@
 // Copyright 2019 The Flutter team. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+class TopAppBarDemo extends StatelessWidget {
+  const TopAppBarDemo({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(
+          Icons.tag_faces,
+        ),
+        title: const Text(
+          'Sample title',
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(
+              Icons.directions_bike,
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(
+              Icons.directions_bus,
+            ),
+            onPressed: () {},
+          ),
+          PopupMenuButton<Text>(
+            itemBuilder: (context) {
+              return [
+                const PopupMenuItem(
+                  child: Text(
+                    'Boat',
+                  ),
+                ),
+                const PopupMenuItem(
+                  child: Text(
+                    'Train',
+                  ),
+                ),
+              ];
+            },
+          )
+        ],
+      ),
+      body: const Center(
+        child: Text(
+          'Hello',
+        ),
+      ),
+    );
+  }
+}
