69d412a87ce8fe0f3dfa54feac4e0f80292838a1
Tianguang Zhang <penn.zht@gmail.com>
2020-11-14 02:09:05 +0100

    Remove imports on flare.

--- lib/pages/backdrop.dart
+++ lib/pages/backdrop.dart
@@ -2,10 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flare_dart/math/mat2d.dart';
-import 'package:flare_flutter/flare.dart';
-import 'package:flare_flutter/flare_actor.dart';
-import 'package:flare_flutter/flare_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
