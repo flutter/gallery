a1e5bc6ef998972438f4e69a5b353128072ef9e9
Renzo Olivares <rmolivares@renzo-olivares.dev>
2021-02-22 00:05:40 -0800

    Revert "[Reply] Cap body at 1340 for desktop and tablet (#425)"
    This reverts commit cb10d4ae4f64b789d749001d075bac6a74f8113b.

--- lib/studies/reply/adaptive_nav.dart
+++ lib/studies/reply/adaptive_nav.dart
@@ -243,14 +243,9 @@ class _DesktopNavState extends State<_DesktopNav>
           ),
           const VerticalDivider(thickness: 1, width: 1),
           Expanded(
-            child: Center(
-              child: ConstrainedBox(
-                constraints: const BoxConstraints(maxWidth: 1340),
-                child: _SharedAxisTransitionSwitcher(
-                  defaultChild: _MailNavigator(
-                    child: MailboxBody(key: widget.inboxKey),
-                  ),
-                ),
+            child: _SharedAxisTransitionSwitcher(
+              defaultChild: _MailNavigator(
+                child: MailboxBody(key: widget.inboxKey),
               ),
             ),
           ),
