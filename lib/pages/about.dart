fc1e7a210e5b0b20069a870c723bd6f956d6d1ee
Pierre-Louis <6655696+guidezpl@users.noreply.github.com>
2021-05-25 14:46:20 +0000

    Use package_info_plus (#511)

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -5,7 +5,7 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/gallery_localizations.dart';
-import 'package:package_info/package_info.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 void showAboutDialog({
