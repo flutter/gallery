8ba5184a5368b7ed1053030dd1acbef00234c52d
Oleg Zaim <106600432+OlegZaimNLT@users.noreply.github.com>
2022-06-08 13:00:09 +0300

    Added missing cupertino picker (#717)
    * Added missing cupertino picker
    
    * Fix
    
    * Update intl_en.arb
    
    * Update intl_en_US.xml
    
    Co-authored-by: Pierre-Louis <6655696+guidezpl@users.noreply.github.com>

--- lib/demos/cupertino/cupertino_picker_demo.dart
+++ lib/demos/cupertino/cupertino_picker_demo.dart
@@ -27,6 +27,17 @@ class _CupertinoPickerDemoState extends State<CupertinoPickerDemo> {
   // Value that is shown in the date picker in dateAndTime mode.
   DateTime dateTime = DateTime.now();
 
+  int _selectedWeekday = 0;
+
+  static List<String> getDaysOfWeek([String? locale]) {
+    final now = DateTime.now();
+    final firstDayOfWeek = now.subtract(Duration(days: now.weekday - 1));
+    return List.generate(7, (index) => index)
+        .map((value) => DateFormat(DateFormat.WEEKDAY, locale)
+            .format(firstDayOfWeek.add(Duration(days: value))))
+        .toList();
+  }
+
   void _showDemoPicker({
     required BuildContext context,
     required Widget child,
@@ -178,6 +189,53 @@ class _CupertinoPickerDemoState extends State<CupertinoPickerDemo> {
     );
   }
 
+  Widget _buildPicker(BuildContext context) {
+    final locale = GalleryLocalizations.of(context)?.localeName;
+    final daysOfWeek = getDaysOfWeek(locale);
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      child: GestureDetector(
+        onTap: () {
+          _showDemoPicker(
+            context: context,
+            child: _BottomPicker(
+              child: CupertinoPicker(
+                backgroundColor:
+                    CupertinoColors.systemBackground.resolveFrom(context),
+                itemExtent: 32.0,
+                magnification: 1.22,
+                squeeze: 1.2,
+                useMagnifier: true,
+                // This is called when selected item is changed.
+                onSelectedItemChanged: (selectedItem) {
+                  setState(() {
+                    _selectedWeekday = selectedItem;
+                  });
+                },
+                children: List<Widget>.generate(daysOfWeek.length, (index) {
+                  return Center(
+                    child: Text(
+                      daysOfWeek[index],
+                    ),
+                  );
+                }),
+              ),
+            ),
+          );
+        },
+        child: _Menu(
+          children: [
+            Text(GalleryLocalizations.of(context)!.demoCupertinoPicker),
+            Text(
+              daysOfWeek[_selectedWeekday],
+              style: const TextStyle(color: CupertinoColors.inactiveGray),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
@@ -195,6 +253,7 @@ class _CupertinoPickerDemoState extends State<CupertinoPickerDemo> {
             _buildTimePicker(context),
             _buildDateAndTimePicker(context),
             _buildCountdownTimerPicker(context),
+            _buildPicker(context),
           ],
         ),
       ),
