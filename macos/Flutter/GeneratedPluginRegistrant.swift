d6b65bb5f90f1cad25c481ba2db790f641956c75
Pierre-Louis Guidez <plg@google.com>
2023-02-10 11:20:11 +0100

    podfiles

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import package_info_plus
-import path_provider_macos
+import path_provider_foundation
 import shared_preferences_foundation
 import url_launcher_macos
 
