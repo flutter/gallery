8693f925f6c7c14d5c19ae378ddfb725257dc4fc
Pierre-Louis <6655696+guidezpl@users.noreply.github.com>
2023-02-22 07:32:32 +0100

    Prepare 2.10 release (#905)
    * pubspec cleanup
    
    * x
    
    * x
    
    * update tag
    
    * Revert "update tag"
    
    This reverts commit e13f7dbc18a4e5e501fc1e8ff19784017643be34.

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,9 +4,11 @@
 
 // Only put constants shared between files here.
 
-// height of the 'Gallery' header
 import 'dart:typed_data';
 
+import 'package:transparent_image/transparent_image.dart' as transparent_image;
+
+// Height of the 'Gallery' header
 const double galleryHeaderHeight = 64;
 
 // The font size delta for headline4 font.
@@ -26,11 +28,11 @@ const Duration halfSplashPageAnimationDuration = Duration(milliseconds: 150);
 
 // Duration for settings panel to open on mobile.
 const Duration settingsPanelMobileAnimationDuration =
-    Duration(milliseconds: 600);
+    Duration(milliseconds: 200);
 
 // Duration for settings panel to open on desktop.
 const Duration settingsPanelDesktopAnimationDuration =
-    Duration(milliseconds: 200);
+    Duration(milliseconds: 600);
 
 // Duration for home page elements to fade in.
 const Duration entranceAnimationDuration = Duration(milliseconds: 200);
@@ -39,74 +41,4 @@ const Duration entranceAnimationDuration = Duration(milliseconds: 200);
 const double firstHeaderDesktopTopPadding = 5.0;
 
 // A transparent image used to avoid loading images when they are not needed.
-// TODO(guidezpl): Replace with transparent_image when
-//https://github.com/brianegan/transparent_image/pull/13 is merged.
-final Uint8List kTransparentImage = Uint8List.fromList(<int>[
-  0x89,
-  0x50,
-  0x4E,
-  0x47,
-  0x0D,
-  0x0A,
-  0x1A,
-  0x0A,
-  0x00,
-  0x00,
-  0x00,
-  0x0D,
-  0x49,
-  0x48,
-  0x44,
-  0x52,
-  0x00,
-  0x00,
-  0x00,
-  0x01,
-  0x00,
-  0x00,
-  0x00,
-  0x01,
-  0x08,
-  0x06,
-  0x00,
-  0x00,
-  0x00,
-  0x1F,
-  0x15,
-  0xC4,
-  0x89,
-  0x00,
-  0x00,
-  0x00,
-  0x0A,
-  0x49,
-  0x44,
-  0x41,
-  0x54,
-  0x78,
-  0x9C,
-  0x63,
-  0x00,
-  0x01,
-  0x00,
-  0x00,
-  0x05,
-  0x00,
-  0x01,
-  0x0D,
-  0x0A,
-  0x2D,
-  0xB4,
-  0x00,
-  0x00,
-  0x00,
-  0x00,
-  0x49,
-  0x45,
-  0x4E,
-  0x44,
-  0xAE,
-  0x42,
-  0x60,
-  0x82,
-]);
+final Uint8List kTransparentImage = transparent_image.kTransparentImage;
