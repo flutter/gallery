e1289d43f508f273d445a6c145cb2b064b50d3c3
Pierre-Louis Guidez <plg@google.com>
2023-04-26 09:44:12 +0200

    add smoke test

---
+++ test/widget_test.dart
@@ -0,0 +1,13 @@
+// Copyright 2019 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:gallery/main.dart';
+
+void main() {
+  testWidgets('Smoke test', (tester) async {
+    await tester.pumpWidget(const GalleryApp());
+  });
+}
