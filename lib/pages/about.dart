bf556eade6b3066774a8af544a791c33dd9941d5
Jonah Williams <jonahwilliams@google.com>
2023-10-31 14:07:13 -0700

    Delete firebase dependencies to simplify framework roll. (#1023)
    We're having trouble updating the flutter gallery to head for flutter
    integration testing. Since we're not updating the gallery any further, I
    believe we should simplify the problem by removing the firebase
    depenencies, which are complicating the native build.
    
    This would ultimately be required to merge this gallery into
    flutter/flutter anyway.

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -5,7 +5,6 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/gallery_localizations.dart';
-import 'package:package_info_plus/package_info_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 void showAboutDialog({
@@ -20,8 +19,7 @@ void showAboutDialog({
 }
 
 Future<String> getVersionNumber() async {
-  final packageInfo = await PackageInfo.fromPlatform();
-  return packageInfo.version;
+  return '2.10.2+021002';
 }
 
 class _AboutDialog extends StatelessWidget {
